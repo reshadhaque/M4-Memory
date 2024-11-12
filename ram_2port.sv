60dd59b9d6f7900edd2bdb2a1d6975f932e52f51
Reshad Haque <reshadulhaque1@gmail.com>
2024-11-12 14:58:35 -0800

    Updated ram

--- ram_2port.sv
+++ ram_2port.sv
@@ -4,11 +4,24 @@ module ram_2port
     input logic [ADDR_WIDTH-1:0] r_addr; //reading address
     input logic [ADDR_WIDTH-1:0] w_addr; //writing address
     input logic [DATA_WIDTH-1:0] w_data;
-    input logic we;
+    input logic write_enable;
     input logic clk;
     output logic [DATA_WIDTH-1:0] r_data;
 );
 
 //
+    logic [DATA_WIDTH-1:0] memory [0:2**ADDR_WIDTH-1];
+
+//Write operation
+    always_ff@(posedge clk)
+    begin
+        if(write_enable)
+            memory[w_addr] <= w_data;
+    end
+
+//Read operation
+
+    assign r_data = memory[r_addr];
+
 
 endmodule
