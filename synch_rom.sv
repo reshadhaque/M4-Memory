1e2cd24d6a4abe27ad43a014f397e922d4cfc0fd
Reshad Haque <reshadulhaque1@gmail.com>
2024-11-13 23:16:03 -0800

    Updated files

--- synch_rom.sv
+++ synch_rom.sv
@@ -3,19 +3,27 @@
 //Coded into the FPGA fabric, cannot be instatiated like Block Ram automatically
 //We cannot write to ROM, we can only read from it
 
-//We can force it to use Block Ram (Bram) by hacking our way through it in (in synchronous rom HDL file)
+//We can force ROM to use Block Ram but:
+//ROM is usually not synchronous, but we need to specify a clock if we want to utilize Block Ram for the ROM
 
 
-module rom_with_file(
+module synch_rom(
+    input logic clk,
     input logic [2:0] addr,
     output logic [1:0] data
 );
 
-    logic [1:0] rom [0:7]; //2**3-1 (2**ADDR_WIDTH-1)
+    (*rom_style = "block" *) logic [1:0] rom [0:7]; //2**3-1 (2**ADDR_WIDTH-1) 
+    //^^^^^^^
+    //Quartus and Vivado will not use Bram because this ROM is very small, so we use this "Synthesis Arrtibute"
 
     //Quartus and Vivado only accepts this for Read Only Memory, Otherwise it is NOT synthesizeable
     initial
         $readmemb("truth_table.mem", rom); //Reads a binary file of memory called truth_table.mem and puts it into the rom //We can also make it a hex file with readmemh
-    assign data = rom[addr];
+    
+    always_ff@(posedge clk)
+        begin
+            assign data = rom[addr];
+        end
 
 endmodule
