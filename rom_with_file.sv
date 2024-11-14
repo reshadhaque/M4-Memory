c535bb025494bc583266c74b61dc58bae860f75a
Reshad Haque <reshadulhaque1@gmail.com>
2024-11-13 23:10:21 -0800

    Updated files

--- rom_with_file.sv
+++ rom_with_file.sv
@@ -1,7 +1,10 @@
 //ROM (Read Only Memory)
 //ROM is essentially a really large lookup table (LUT)
-//Coded into the FPGA fabric, cannot be instatiated like Block Ram
+//Coded into the FPGA fabric, cannot be instatiated like Block Ram automatically
 //We cannot write to ROM, we can only read from it
+
+//We can force it to use Block Ram (Bram) 
+
 
 module rom_with_file(
     input logic [2:0] addr,
@@ -12,7 +15,7 @@ module rom_with_file(
 
     //Quartus and Vivado only accepts this for Read Only Memory, Otherwise it is NOT synthesizeable
     initial
-        $readmemb("truth_table.mem", rom); //Reads a binary file of memory called truth_table.mem and puts it into the rom
+        $readmemb("truth_table.mem", rom); //Reads a binary file of memory called truth_table.mem and puts it into the rom //We can also make it a hex file with readmemh
     assign data = rom[addr];
 
 endmodule
